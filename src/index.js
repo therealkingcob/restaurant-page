071567578ffc64fb994980eb00fbba55481325e8
therealkingcob <rishiashah19@gmail.com>
2024-05-12 11:36:05 -0700

    get the buttons in js and give them event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import createPage from './loader'
 
+const homeButton = document.getElementById("home");
+const scheduleButton = document.getElementById("schedule");
+const contactButton = document.getElementById("contact");
+
 createPage();
+
+homeButton.addEventListener("click");
+scheduleButton.addEventListener("click");
+contactButton.addEventListener("click");
+
